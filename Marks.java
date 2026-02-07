dbdb5bb31f344d42086d8e7f538af6b5b09fc6dc
Sumithra <sumithra96523@gmail.com>
2026-02-07 11:05:37 +0530

    Add files via upload

---
+++ Marks.java
@@ -0,0 +1,14 @@
+class Marks
+{
+    public static void main(String args[])
+    {
+        int maths=98;
+        int science=96;
+        int english=99;
+        int total,average;
+        total=maths+science+english;
+        average=total/3;
+        System.out.println("Total marks:"+total);
+        System.out.println("Average:"+average);
+    }
+}
