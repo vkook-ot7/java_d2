87b417a27e9817b2fab7919c213a501c5ce919fe
Sumithra <sumithra96523@gmail.com>
2026-02-07 10:45:16 +0530

    Add files via upload

---
+++ Jjk.java
@@ -0,0 +1,14 @@
+class Jjk
+{
+    public static void main(String args[])
+    {
+        string name='JJK';
+        double marks=99;
+        int age=28;
+        boolean status=TRUE;
+        System.out.println(name);
+        System.out.println(marks);
+        System.out.println(age);
+        System.out.println(status);
+    }
+}
