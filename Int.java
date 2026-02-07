57c0800cd63ee34ea608403416447f0bbcdfb2df
Sumithra <sumithra96523@gmail.com>
2026-02-07 10:36:29 +0530

    Add files via upload

---
+++ Int.java
@@ -0,0 +1,18 @@
+class Int
+{
+    public static void main(String args[])
+    {
+        int a=10;//decimal
+        int b=010;//octal
+        int c=0x1a;//hexa
+
+        int d=0b111;//binary
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+
+
+        
+    }
+}
