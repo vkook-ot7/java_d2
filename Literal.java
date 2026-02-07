7119669547da8e5ba218290fc414f5a1b4cf4894
Sumithra <sumithra96523@gmail.com>
2026-02-07 11:41:22 +0530

    Add files via upload

---
+++ Literal.java
@@ -0,0 +1,27 @@
+class Literal
+{
+    public static void main(String args[])
+    {
+         int a=10;//decimal
+        int b=010;//octal
+        int c=0x1a;//hexa
+        int d=0b111;//binary 
+        float i=10.67f;//float
+        double f=19.34d;//double
+        char g='v';//character
+        boolean h=true;//boolean
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(i);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(h);
+
+
+
+
+
+    }
+}
