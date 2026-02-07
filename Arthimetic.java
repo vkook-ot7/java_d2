7679a4831c861806352f04653cc1c16771b818b1
Sumithra <sumithra96523@gmail.com>
2026-02-07 11:52:44 +0530

    Add files via upload

---
+++ Arthimetic.java
@@ -0,0 +1,15 @@
+class Arthimetic
+{
+    public static void main(String args[])
+    {
+        int a,b;
+        a=12;
+        b=23;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+        
+    }
+}
