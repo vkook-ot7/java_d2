de7b09c079f57a82519cb1ad1d641b5507c13708
Sumithra <sumithra96523@gmail.com>
2026-02-07 14:20:34 +0530

    Add files via upload

---
+++ Shift.java
@@ -0,0 +1,14 @@
+class Shift
+{
+    public static void main(String args[])
+    {
+        int a,b;
+        a=5;
+        b=2;
+        System.out.println(a>>b);
+        System.out.println(a<<b);
+        System.out.println(b<<a);
+        System.out.println(b>>a);
+       
+    }   
+}
