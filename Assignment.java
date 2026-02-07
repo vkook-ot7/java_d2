a5342d4e02711c4524ebe6bd8be03024d617704e
Sumithra <sumithra96523@gmail.com>
2026-02-07 14:12:19 +0530

    Add files via upload

---
+++ Assignment.java
@@ -0,0 +1,14 @@
+class Assignment
+{
+    public static void main(String args[])
+    {
+        int a,b;
+        a=5;
+        b=2;
+        System.out.println(a+=b);//7
+        System.out.println(a-=b);//5
+        System.out.println(a*=b);//10
+        System.out.println(a/=b);//5
+        System.out.println(a%=b);//1
+    }   
+}
