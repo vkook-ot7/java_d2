c2b32d13c5c8efb0d01eae1367aa16b7f0a530b9
Sumithra <sumithra96523@gmail.com>
2026-02-07 15:17:54 +0530

    Add files via upload

---
+++ Swap.java
@@ -0,0 +1,14 @@
+class Swap
+{
+    public static void main(String args[])
+    {
+        int a=2;
+        int b=3;
+        int temp;
+        temp =a;
+        a=b;
+        b=temp;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
